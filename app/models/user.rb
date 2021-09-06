b35b18ee59f91cba5ad3ded4e6f88a8f4c981a6c
SizweMalobola <25reyals@gmail.com>
2021-09-06 16:31:06 +0200

    testing layout links

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ApplicationRecord
   has_secure_password
   # its password and not password_digest because this isn't the database
   validates :password, presence: true, length: { minimum: 6 }
+  # Returns the hash digest of the given string.
+  def User.digest(string) 
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+    BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
